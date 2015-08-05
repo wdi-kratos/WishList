be8bbe786f5e486921a2ecc679fa0ac615980994
Christopher Kim <ckim@CKim-MacBook-Pro.local>
2015-08-05 00:29:20 -0500

    appcontroller updated

--- controllers/ItemsController.rb
+++ controllers/ItemsController.rb
@@ -1,15 +1,83 @@
 class ItemsController < ApplicationController
 
 
-# Create a new list item
-get '/' do
-erb :index
-end
+  # Create a new list item
+  get '/' do
 
-post '/create' do
-erb :item_create
-end
+    authorization_check
 
-# Edit an existing Item
+    @item = ItemsModel.all
+    erb :index
+  end
+
+  ## create a new item
+  get '/create' do
+
+    authorization_check
+
+    erb :create
+  end
+
+  post '/create' do
+
+    authorization_check
+    @message = ''
+
+    @item = ItemsModel.new
+    @item.title = params[:title]
+    @item.description = params[:description]
+    @item.color = params[:color]
+    @item.size = params[:size]
+    @item.condition = params[:condition]
+    @item.location = params[:location
+    @item.save
+
+    @message = 'has been sucessfully added to your list!'
+    erb :item_notification
+  end
+
+  ## update (edit) items on the list individually. Bonus -> enhance UI via AJAX?
+  get '/edit/:id' do
+
+    authorization_check
+
+    @id = params[:id]
+    @item = ItemsModel.find(@id)
+
+    erb :item_edit
+  end
+
+  post '/edit' do
+
+    authorization_check
+    @message = ''
+
+    @item = ItemsModel.find(params[:id])
+    @item.title = params[:title]
+    @item.description = params[:description]
+    @item.color = params[:color]
+    @item.size = params[:size]
+    @item.condition = params[:condition]
+    @item.location = params[:location
+    @item.save
+
+    @message = 'has been sucessfully modified from your list!'
+    erb :item_notification
+
+  end
+
+  ## delete list record
+  post '/delete' do
+
+    authorization_check
+    @message = ''
+
+    @id = params[:id]
+    @item = ItemsModel.find(@id)
+    @item.destroy
+
+    @message = 'has been sucessfully removed from your list!'
+    erb :item_notification
+  end
 
 end
