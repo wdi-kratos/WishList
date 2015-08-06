2be82162ce05b4634bbb1c0fdf6be8a4e00b4d0a
TristanMarshall <tristanmarshall@ymail.com>
2015-08-06 09:04:08 -0500

    css

--- public_dir/scripts/apps.js
+++ public_dir/scripts/apps.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
 $(".want").fadeIn(1500);
+// $(".derphoto").fadeIn(1500);
+// $(".logophoto").delay(1000).fadeIn(1500);
+// $(".buttonbar").delay(1000).fadeIn(1500);
 
 $(".catdrop").click(function() {
   $(".itemform").slideDown(1000);
